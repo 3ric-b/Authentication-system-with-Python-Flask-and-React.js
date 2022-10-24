e4f33fa03e3665d0a47530404e6892d99cdabf22
b5720 <baez5720@outlook.com>
2022-10-24 16:50:05 +0000

    hecho

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,27 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+    # Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
+
+
+@api.route('/hello', methods=[ 'GET'])
+@jwt_required()
+def get_hello():
+    email = get_jwt_identity()
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, you are with '" + email + "' identity."
+    }
+
+    return jsonify(response_body), 200
